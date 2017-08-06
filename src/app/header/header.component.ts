37cac2e52b72e72bafbb9f7744d7d05c04494686
Alexander Mykolaichuk <alexmix3000@gmail.com>
2017-08-06 14:49:43 +0300

    conditional views

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  @Output() currentViewEvent = new EventEmitter<string>();
+
+  setView(view: string) {
+    this.currentViewEvent.emit(view);
+  }
 
   constructor() { }
 
